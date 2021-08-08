ada2a9d3aeeea9b20e036744cf12da609cd3f666
cody <cody6750@gmail.com>
2021-08-07 20:33:55 -0700

    Add new functions

--- src/questions/Q3.go
+++ src/questions/Q3.go
@@ -35,3 +35,34 @@ func LengthOfLongestSubstring(s string) int {
 	return longestLength
 
 }
+
+func LengthOfLongestSubstrin1g21(s string) int {
+
+	var longestLength int
+	var count int
+	if len(s) == 1 || len(s) == 0 {
+		fmt.Print("Exit early")
+		return len(s)
+	}
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("Letter is %v\n", string(s[i]))
+		count = 1
+		usedLetters := make(map[string]int)
+		usedLetters[string(s[i])] = 1
+		for j := i + 1; j < len(s); j++ {
+			fmt.Printf("2nd Letter is %v\n", string(s[j]))
+			if usedLetters[string(s[j])] == 0 {
+				usedLetters[string(s[j])] = 1
+				count++
+			} else {
+				break
+			}
+		}
+		if count > longestLength {
+			longestLength = count
+		}
+	}
+	return longestLength
+
+}
--- src/questions/tools.go
+++ src/questions/tools.go
@@ -1,6 +1,9 @@
 package questions
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 //Queue .. Last in First out
 type Queue struct {
@@ -41,4 +44,35 @@ func (s Stack) Pop() int {
 	lastElementValue := s.list[lastElement]
 	s.list = s.list[:lastElement]
 	return lastElementValue
+}
+
+func LengthOfLongestSubstrin1g(s string) int {
+
+	var longestLength int
+	var count int
+	if len(s) == 1 || len(s) == 0 {
+		fmt.Print("Exit early")
+		return len(s)
+	}
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("Letter is %v\n", string(s[i]))
+		count = 1
+		usedLetters := make(map[string]int)
+		usedLetters[string(s[i])] = 1
+		for j := i + 1; j < len(s); j++ {
+			fmt.Printf("2nd Letter is %v\n", string(s[j]))
+			if usedLetters[string(s[j])] == 0 {
+				usedLetters[string(s[j])] = 1
+				count++
+			} else {
+				break
+			}
+		}
+		if count > longestLength {
+			longestLength = count
+		}
+	}
+	return longestLength
+
 }
